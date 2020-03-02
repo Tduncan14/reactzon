230f94f91615f55716249fc86c5d4e0a74767927
Treek <nbatre1@yahoo.com>
2020-03-02 04:59:59 -0600

    keep moving forward

---
+++ frontend/src/Components/Footer.js
@@ -0,0 +1,22 @@
+import React,{Component} from'react';
+
+
+class Footer extends Component {
+
+
+
+  render(){
+
+    return(
+        <footer class="footer">
+        All Rights reserved
+     </footer>
+    )
+  }
+}
+
+
+
+export default Footer
+
+export default Footer;
